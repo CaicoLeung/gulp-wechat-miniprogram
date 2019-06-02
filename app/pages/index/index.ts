ac1a5adc6a705db4bc12df998b19191cb35ddb4e
CaicoLeung <lkk1053190902@gmail.com>
2019-06-02 16:15:52 +0800

    test

--- app/pages/index/index.ts
+++ app/pages/index/index.ts
@@ -1,4 +1,7 @@
 /// <reference path="../../../typings/index.d.ts" />
+import { IMyApp } from '../../app';
+
+const app = getApp<IMyApp>();
 Page({
   data: {
     motto: '点击"编译"以构建',
@@ -12,6 +15,35 @@ Page({
     })
   },
   onLoad() {
-    
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse) {
+      app.userInfoReadyCallback = (res) => {
+        this.setData({
+          userInfo: res,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      wx.getUserInfo({
+        success: (res: wx.GetUserInfoSuccessCallbackResult) {
+          app.globalData.userInfo = res.userInfo;
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+  },
+  getUserInfo(e: any) {
+    app.globalData.userInfo = e.detail.userInfo;
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
   }
 })
