8db36b4a9579dfd56f3bb0375390d49ebda2492c
lzc <443266630@qq.com>
2019-11-22 16:58:01 +0800

    个人中心

--- app/pages/my/myTreasure/index.ts
+++ app/pages/my/myTreasure/index.ts
@@ -1,7 +1,14 @@
 Page({
   data: {
-    
-  },
+    goodsList:[
+      {username:'小唐马'},
+      {username:'小唐马'},
+      {username:'小唐马'},
+      {username:'小唐马'},
+      {username:'小唐马'},
+      {username:'小唐马'},
+    ]
+  }
   onLoad() {
 
   },
