47da2dd2d2164ca24c221816dbaa5fdb99444de0
CaicoLeung <lkk1053190902@gmail.com>
2021-04-30 16:34:49 +0800

    feat: ts编译不做eslint校验

--- gulp/Typescript.ts
+++ gulp/Typescript.ts
@@ -1,20 +1,20 @@
 import { distPath, tsFiles } from './SourcePath'
 import babel from 'gulp-babel'
 import colors from 'colors'
-import eslint from 'gulp-eslint'
+// import eslint from 'gulp-eslint'
 import gulp from 'gulp'
 import gulpTypescript from 'gulp-typescript'
 import sourcemaps from 'gulp-sourcemaps'
 
-const tsConfig = gulpTypescript.createProject('app/tsconfig.json')
+const tsProject = gulpTypescript.createProject('app/tsconfig.json')
 
 export const tsParser = () => {
   return gulp.src(tsFiles, { since: gulp.lastRun(tsParser) })
     .pipe(sourcemaps.init())
-    .pipe(eslint())
-    .pipe(eslint.format())
-    .pipe(eslint.failAfterError())
-    .pipe(tsConfig()).js
+    // .pipe(eslint())
+    // .pipe(eslint.format())
+    // .pipe(eslint.failAfterError())
+    .pipe(tsProject()).js
     .on('error', (err: string) => { console.log(colors.red(err)) })
     .pipe(babel({
       presets: ['@babel/preset-env']
