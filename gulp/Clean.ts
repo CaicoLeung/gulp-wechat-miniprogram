8da584ccd7eb2708634050e86d88ee074c2eaee2
CaicoLeung <lkk1053190902@gmail.com>
2021-06-08 10:15:54 +0800

    fix: cleanDistDir

--- gulp/Clean.ts
+++ gulp/Clean.ts
@@ -1,4 +1,16 @@
 import fs from 'fs'
 import path from 'path'
 
-export const cleanDistDir = async () => fs.rmSync(path.join(process.cwd(), 'dist'), { recursive: true })
+export const cleanDistDir = async () => {
+  const distPath = path.join(process.cwd(), 'dist')
+  try {
+    const stat = fs.statSync(distPath)
+    if (stat.isDirectory()) {
+      fs.rmdirSync(distPath, { recursive: true })
+    } else {
+      fs.mkdirSync(distPath)
+    }
+  } catch (err) {
+    console.error(err)
+  }
+}
